7e5af060477dd0233ef6bc9c39928b48981af923
nizzi0623 <julie870529@gmail,com>
2018-01-02 11:52:00 +0800

    Date

---
+++ app/src/main/java/com/nizzi/atm/Tester.java
@@ -0,0 +1,26 @@
+package com.nizzi.atm;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by user on 2018/1/2.
+ */
+
+public class Tester {
+    public static void main(String[] args){
+        Date now = new Date();
+        System.out.println(now);
+        System.out.println(now.getTime());
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        System.out.println(sdf.format(now));
+        //反過來，有字串，得Date物件?
+        String s = "1998-04-01";
+        try{
+            Date birthday = sdf.parse(s);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+}
